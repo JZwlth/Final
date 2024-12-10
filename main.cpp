fbcc24a95cbc94eb8a8e7a845118e6548474eb5c
JZwlth <wkjazy87@gmail.com>
2024-12-10 10:38:29 -0800

    update

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// For Coffee Booth (Linked List)
+struct ListNode {
+
+};
+
+// For Muffin, Friendship Bracelets, and Donuts booths
+struct Customer {
+};
+
+// Function Prototypes
+
+int main() {
+    return 0;
+}
+
+// Function
+
+void appendCustomer(){
+    
+}
+
