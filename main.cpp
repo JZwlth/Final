8cfb4c7609699211d0100b2876083d3a43eef958
JZwlth <wkjazy87@gmail.com>
2024-12-10 10:58:19 -0800

    update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <deque>
 #include <vector>
+#include <list>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -21,8 +24,30 @@ void appendCustomer();
 int main() {
     srand((unsigned)time(nullptr)); 
     const string names[] = {};
-    const int nameCount = sizeof(names)/size
+    const int nameCount = sizeof(names)/sizeof(names[0]);
 
+    const string coffeeOrders[] = {"l", "c", "a", "e", "m"};
+    const int coffeeOrderCount = sizeof(coffeeOrders)/sizeof(coffeeOrders[0]);
+
+    const string muffinOrders[] = {"b", "c", "b", "l"};
+    const int muffinOrderCount = sizeof(muffinOrders)/sizeof(muffinOrders[0]);
+
+    const string braceletOrders[] = {"bb", "ff", "c v", "ww"};
+    const int braceletOrderCount = sizeof(braceletOrders)/sizeof(braceletOrders[0]);
+
+    const string donutOrders[] = {"ga", "cs", "sd", "as"};
+    const int donutOrderCount = sizeof(donutOrders)/sizeof(donutOrders[0]);
+
+    // Coffee Booth: Linked List
+    ListNode* coffeeHead = nullptr;
+    // Muffin Booth: deque
+    deque<Customer> muffinQueue;
+    // Friendship Bracelet Booth: vector
+    vector<Customer> braceletQueue;
+    // Donut Booth: list
+    list<Customer> donutQueue;
+
+    
     return 0;
 }
 
